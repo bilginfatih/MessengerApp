a976264fe06b572201e09e8f0dce0f27a7e719d3
connetium-fatih <fatih16bilgin@gmail.com>
2022-11-09 14:23:54 +0300

    custom keyboard added

---
+++ Zey/Constants.swift
@@ -0,0 +1,37 @@
+//
+//  Constants.swift
+//  Zey
+//
+//  Created by Fatih Bilgin on 9.11.2022.
+//
+
+import Foundation
+import UIKit
+
+enum Constants{
+    
+    static let keyNormalColour: UIColor = .white
+    static let keyPressedColour: UIColor = .lightText
+    static let specialKeyNormalColour: UIColor = .gray
+
+    static let letterKeys = [
+        ["q", "w", "ʒ", "𝓁", "⤓", "Ψ", "□", "ᕯ", "✿", "þ", "⨁", "◫"],
+        ["⊗", "⧖", "⟥⟤", "∞", "۹", "1", "Ĵ", "⋇", "◺", "ş", "!"],
+        ["⬆️", "⤫", "x", "ℂ", "◊", "♡", "%", "Ω", "ö", "ç", "⌫"],
+        ["123", "🌐", "space", "↩"]
+    ]
+    //۹
+    static let numberKeys = [
+        ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0",],
+        ["-", "/", ":", ";", "(", ")" ,",", "$", "&", "@", "\""],
+        ["#+=",".", ",", "?", "!", "\'", "⌫"],
+        ["ABC", "🌐", "space", "↩"]
+    ]
+    
+    static let symbolKeys = [
+        ["[", "]", "{", "}", "#", "%", "^", "*", "+", "="],
+        ["_", "\\", "|", "~", "<", ">", "€", "£", "¥", "·"],
+        ["123",".", ",", "?", "!", "\'", "⌫"],
+        ["ABC", "🌐", "space", "↩"]
+    ]
+}
